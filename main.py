22d1323f3ac7c933a51df072aeedc2c695cf2780
Welzewool <blackenedima@yandex.ru>
2024-12-06 18:19:22 +0300

    Удалены лишние импорты, функции os заменены на pathlib. Поправлены аннотации (str | None вместо [str, None]). Путь и операции с файлами теперь осуществляются через Path (file.name, destination_path / file.name). Цикл for file in files стал более простым и читабельным.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """Код для сортировки файлов по папкам"""
 # import os
 # import shutil
-from os import listdir, makedirs
-from os.path import isfile, join
+# from os import listdir, makedirs
+# from os.path import isfile, join
 from shutil import move
 from pathlib import Path
 
@@ -27,23 +27,14 @@ destinations = {
 }
 
 
-# Получение списка файлов
-files_in_path = [file for file in listdir(download_path) if isfile(join(download_path, file))]
-if not files_in_path:
-    print("Каталог пуст")
-else:
-    print(f"Каталог содержит {len(files_in_path)} файла(-ов), \nфайлы: {files_in_path}")
-
-
 # disk_usage_download = disk_usage(file_path_download)
 
 def get_file_category(filename: str, extensions_dict: dict[str, str]) -> [str, None]:
     """
-    Функция принимает имя файла и словарь с расширениями, определяя к какой категории файл относится.
-    Если расширение не найден, то файл остается в исходной папке.
-    :param filename:
-    :param extensions_dict:
-    :return:
+    Определяет категорию файла по его расширению.
+    :param filename: Имя файла
+    :param extensions_dict: Словарь категорий и расширений
+    :return: Категория или None
     """
     extension = filename.split(".")[-1].lower()  # Получение расширения файла
     for category, extensions in extensions_dict.items():
@@ -52,29 +43,34 @@ def get_file_category(filename: str, extensions_dict: dict[str, str]) -> [str, N
     return None
 
 
-def sort_files(files: list[str], source_path, destinations_path, extensions_dict: dict[str, str]):
+def sort_files(files: list[Path], source_path: Path, destinations_dct: dict[str, Path], extensions_dict: dict[str, str]):
     """
-    Функция определяет категорию каждого файла, перемешает его в соответсвующую папку
-    :param files:
-    :param source_path:
-    :param destinations_path:
-    :param extensions_dict:
-    :return:
+    Перемещает файлы в соответствующие папки на основе их категории.
+    :param files: Список файлов
+    :param source_path: Путь к исходной папке
+    :param destinations_dct: Словарь категорий и путей
+    :param extensions_dict: Словарь категорий и расширений
     """
     for file in files:
-        category = get_file_category(file, extensions_dict)
-        if category and category in destinations_path:
-            destination_path = destinations[category]
-            makedirs(destination_path, exist_ok=True)  # создает папку, если её нет
+        category = get_file_category(file.name, extensions_dict)
+        if category and category in destinations_dct:
+            destination_path = destinations_dct[category]
+            destination_path.mkdir(parents=True, exist_ok=True)  # создает папку, если её нет
             try:
-                move(join(source_path, file), join(destination_path, file))     # перемещает файл
+                move(str(file), str(destination_path / file.name))     # перемещает файл
                 print(f"Файл {file} был перемещен в папку {destination_path}")
             except Exception as e:
-                print(f"Ошибка при перемещении файла: {e}")
+                print(f"Ошибка при перемещении файла {file.name}: {e}")
         else:
-            print(f"Файл {file} оставлен в {source_path}, не определено расширение файла")
+            print(f"Файл {file.name} оставлен в {source_path}, не определено расширение файла")
 
 
 if __name__ == '__main__':
-    sort_files(files_in_path, download_path, destinations, file_extensions)
+    # Получение списка файлов
+    files_in_path = [file for file in download_path.iterdir() if file.is_file()]
+    if not files_in_path:
+        print("Каталог пуст")
+    else:
+        print(f"Каталог содержит {len(files_in_path)} файла(-ов): {[file.name for file in files_in_path]}")
+        sort_files(files_in_path, download_path, destinations, file_extensions)
 
