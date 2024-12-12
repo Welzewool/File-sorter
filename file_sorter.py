d287720e80fa6304d4f994e299deeb9c6df9d812
Welzewool <blackenedima@yandex.ru>
2024-12-12 11:10:59 +0300

    file_sorter главный файл отвечает за запуск скрипта, основные функции перемещены в папку scr

--- file_sorter.py
+++ file_sorter.py
@@ -1,67 +1,17 @@
 """Код для сортировки файлов по папкам"""
 import time
-from shutil import move
 from pathlib import Path
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-from src.file_category import get_file_category
-from src.extensions import file_extensions
-
-
-home_directory = Path.home()  # Путь к домашнему каталогу
-# подкаталоги относительно домашней папки
-download_path = home_directory / "Загрузки"
-
-images_path = home_directory / "Изображения"
-music_path = home_directory / "Музыка"
-video_path = home_directory / "Видео"
-documents_path = home_directory / "Документы"
-
-destinations = {
-"image": images_path,
-"audio": music_path,
-"video": video_path,
-"documents": documents_path,
-}
-
-# disk_usage_download = disk_usage(file_path_download)
-
-def sort_files(file_path: Path):
-    """
-    Перемещает файлы в соответствующие папки на основе их категории.
-    """
-    category = get_file_category(file_path.name, file_extensions)
-    if category and category in destinations:
-        destination_path = destinations[category]
-        destination_path.mkdir(parents=True, exist_ok=True)
-        try:
-            move(str(file_path), str(destination_path / file_path.name))
-            print(f"Файл {file_path.name} перемещён в {destination_path}")
-        except Exception as e:
-            print(f"Ошибка при перемещении файла {file_path.name}: {e}")
-    else:
-        print(f"Файл {file_path.name} оставлен в {download_path}: расширение не определено")
-
-
-class FileHandler(FileSystemEventHandler):
-    """
-    Класс для обработки файловых событий в папке.
-    """
-    def on_created(self, event):
-        """
-        Отслеживает появление нового файла.
-        Проверяет, что это файл, а не папка, и вызывает функцию сортировки
-        """
-        if not event.is_directory:
-            file_path = Path(event.src_path)
-            sort_files(file_path)
+from src.event_handler import FileHandler
 
 
 if __name__ == '__main__':
-    event_handler = FileHandler()  # Обработчик событий
+    home_directory = Path.home()
+    download_path = home_directory / "Загрузки"
+
+    event_handler = FileHandler(download_path)  # Обработчик событий
     observer = Observer()  # Объект событий, следит за папкой
-    observer.schedule(event_handler, str(download_path), recursive=False)  # Запуск обработчика событий
+    observer.schedule(event_handler, str(download_path), recursive=False)  # Настройка обработчика событий
 
     try:
         print(f"Наблюдение за папкой {download_path} запущено...")
@@ -71,4 +21,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Остановка скрипта")
         observer.stop()
-    observer.join()  #
+    observer.join()
